79747bdac0db2054de1ffa7c84c636082674875b
Edju03 <90040359+Edju03@users.noreply.github.com>
2024-04-30 14:12:59 -0700

    Update dynamixel_controller.py

--- src/dynamixel_controller.py
+++ src/dynamixel_controller.py
@@ -128,8 +128,6 @@ def read_write_py_node(params_dict):
         yaml.safe_dump(params_dict, f)
         rospy.Subscriber('/leg_heights', legHeights, set_goal_pos_callback, f, queue_size=1)
         rospy.Service('get_position', GetPosition, get_present_pos)
-        for l in f:
-            print(l)
         rospy.spin()
 
 def main():
